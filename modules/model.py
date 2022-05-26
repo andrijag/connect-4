da1d6084b266c7c910f215f9a22ce61a30e7ec25
Andrija Grozdanović <andrija.gro@gmail.com>
2022-05-26 21:59:40 +0200

    column moves

--- modules/model.py
+++ modules/model.py
@@ -21,8 +21,8 @@ class Game(Subject):
         self.players = [Player(i) for i in range(1, n_players + 1)]
         self._iterator = cycle(self.players)
         self.player = next(self._iterator)
-        self.board = Board(n_rows, n_columns)
-        self._validator = Validator(self.board, connect_n)
+        self.grid = Grid(n_rows, n_columns)
+        self._validator = Validator(self.grid, connect_n)
         self.game_over = False
         self._observers = []
 
@@ -38,20 +38,19 @@ class Game(Subject):
 
     def drop(self, i):
         if self._legal_move(i):
-            self.player.drop(self.board, i)
-            if self.winning_move(i):
+            self.player.drop(self.grid, i)
+            if self._winning_move(i):
                 self._end_game()
             else:
                 self._next_turn()
             self.notify_observers()
 
     def _legal_move(self, i):
-        return not self.game_over and not self.board[i][-1]
+        return not self.game_over and not self.grid[i][-1]
 
-    def winning_move(self, i):
-        for j in reversed(range(self.board.n_rows)):
-            if self.board[i][j]:
-                print(j)
+    def _winning_move(self, i):
+        for j in reversed(range(self.grid.n_rows)):
+            if self.grid[i][j]:
                 return self._validator.check(i, j)
 
     def _end_game(self):
@@ -64,7 +63,7 @@ class Game(Subject):
     def restart(self):
         self._iterator = cycle(self.players)
         self.player = next(self._iterator)
-        self.board.reset()
+        self.grid.reset()
         self.game_over = False
         self.notify_observers()
 
@@ -77,37 +76,37 @@ class Player:
     def __str__(self):
         return f"player {self.id_}"
 
-    def drop(self, board, i):
-        board.stack(i, self.id_)
+    def drop(self, grid, i):
+        grid.stack(i, self.id_)
 
 
-class Board:
+class Grid:
     def __init__(self, n_rows, n_columns):
-        self._board = [[0 for _ in range(n_rows)] for _ in range(n_columns)]
+        self._grid = [[0 for _ in range(n_rows)] for _ in range(n_columns)]
         self.n_rows = n_rows
         self.n_columns = n_columns
 
     def __getitem__(self, key):
-        return self._board[key]
+        return self._grid[key]
 
     def __str__(self):
-        return str(self._board)
+        return str(self._grid)
 
     def reset(self):
-        for i in range(self.n_rows):
-            for j in range(self.n_columns):
-                self._board[i][j] = 0
+        for i in range(self.n_columns):
+            for j in range(self.n_rows):
+                self._grid[i][j] = 0
 
     def stack(self, i, value):
         for j in range(self.n_rows):
-            if not self._board[i][j]:
-                self._board[i][j] = value
+            if not self._grid[i][j]:
+                self._grid[i][j] = value
                 break
 
 
 class Validator:
-    def __init__(self, board, connect_n):
-        self.board = board
+    def __init__(self, matrix, connect_n):
+        self.matrix = matrix
         self.connect_n = connect_n
         self._vectors = {
             "horizontal": (0, 1),
@@ -128,9 +127,9 @@ class Validator:
 
     def _count_consecutive(self, i, j, di, dj):
         if (
-            i + di in range(self.board.n_rows)
-            and j + dj in range(self.board.n_columns)
-            and self.board[i][j] == self.board[i + di][j + dj]
+            i + di in range(self.matrix.n_columns)
+            and j + dj in range(self.matrix.n_rows)
+            and self.matrix[i][j] == self.matrix[i + di][j + dj]
         ):
             return 1 + self._count_consecutive(i + di, j + dj, di, dj)
         return 1
