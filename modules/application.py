eeda42946146d0c61e13ceb038f4b3aac76020c5
andrija <andrija.gro@gmail.com>
2024-10-23 23:08:15 +0200

    replace grid with pack layout manager

--- modules/application.py
+++ modules/application.py
@@ -12,8 +12,6 @@ class Application(tk.Tk):
     def __init__(self) -> None:
         super().__init__()
         self.title("Connect 4")
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
 
         model = Game(N_ROWS, N_COLUMNS, CONNECT_N)
         view = View(self, model)
@@ -21,4 +19,4 @@ class Application(tk.Tk):
         model.attach_observer(view)
         model.notify_observers()
 
-        view.grid(column=0, row=0, sticky="nsew")
+        view.pack(expand=True, fill="both")
